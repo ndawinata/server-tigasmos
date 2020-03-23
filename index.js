123ea99ccc01c7fedda84be827de4fd8b2d64fb9
Nanda Winata <41312762+ndawinata@users.noreply.github.com>
2020-03-23 18:42:43 +0700

    mengisi index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import Route from './route'
+
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+const port = 5000
+
+// izinkan Cors
+app.use((request, response, next) => {
+    response.header('Access-Control-Allow-Origin', '*')
+    response.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept')
+    next();
+})
+
+// set bodyParser sebagai middleware yang akan memparsing body request
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+// connect ke database
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/server-tigasmos', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+// set route
+app.use('/api', Route)
+app.get('/', (request, response) => {
+    return response.end('Api Working')
+})
+
+server.listen(port, () => {
+    console.log(`berjalan di port ${port}`)
+})
