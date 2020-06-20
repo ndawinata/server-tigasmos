e943c60f9108b1c74eaa66c508875b27b26e0c29
ndawinata <nanda.winata@gmail.com>
2020-06-20 22:33:44 +0700

    ganti tanggal potensi 1

--- src/controller.js
+++ src/controller.js
@@ -164,7 +164,8 @@ export const addsite_1 = (request, response) => {
 
                 tanggal = obj.Infotsunami.Gempa.Tanggal
                 // uncomment dibawah untuk custom tgl potensi tsunami
-                tanggal = '20-Jun-20'
+                // tanggal = '20-Jun-20'
+                tanggal = moment().format('DD-MMM-YY')
                 // latPot = obj.Infotsunami.Gempa.Lintang
                 // lonPot = obj.Infotsunami.Gempa.Bujur
                 // tesing uncomment kode dibawah ini
@@ -202,7 +203,7 @@ export const addsite_1 = (request, response) => {
         newPasut = newData.pasut_sensor_ultrasonik
         dateNow = moment().format('DD-MMM-YY')
         // tesing uncomment kode dibawah
-        dateNow = "03-Apr-20"
+        // dateNow = "03-Apr-20"
         // ---------------
         datePotensi = tanggal
 
